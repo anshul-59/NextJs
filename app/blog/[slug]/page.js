daf7e9d75ea9579c1e3376c95a6de18c31032a6a
anshul-59 <anshul.bansod5901@gmail.com>
2024-02-11 13:47:46 +0530

    Demo project done

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function BlogPostPage() {
+  return (
+    <main>
+      <h1>BlogPostPage</h1>
+    </main>
+  );
+}
---
+++ app/blog/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function BlogPage() {
+  return (
+    <main>
+      <h1>BlogPage</h1>
+      <p>
+        <Link href="/blog/post-1">Blog post -1</Link>
+      </p>
+      <p>
+        <Link href="/blog/post-2">Blog post -2</Link>
+      </p>
+    </main>
+  );
+}
